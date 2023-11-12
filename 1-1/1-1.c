184e82b21e674594b2926389960b88c63c0b74f2
lcq22 <3601611747@qq.com>
2023-11-12 20:35:16 +0800

    11.12

---
+++ 1-1/1-1.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>				/*编译预处理指令*/
+int main()						/*主函数的函数头*/
+{								/*函数体的开始标记*/
+	printf("你好，C语言！\n");	/*输出要显示的字符串*/
+	return 0;					/*函数返回值0*/
+}								/*函数的结束标记*/
---
+++ 11_12/11-12.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>								/*编译预处理指令*/
+int main(void)									/*主函数的函数头*/
+{												/*函数体的开始标志*/
+	printf("***这是我的第一个C语言程序！***\n");/*输出要显示的字符串*/
+
+	return 0;									/*程序返回值*/
+}												/*函数的结束标志*/
